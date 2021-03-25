037b540ccb17c1ce4c776274648ae5e02456cbd4
Noah Doersing <git@noahdoersing.com>
2021-03-25 21:26:11 +0100

    Enable navigating the search results using the keyboard (addresses part of #1)

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,8 +1,12 @@
 const searchInput = document.querySelector("#search_input");
 const searchOutput = document.querySelector("#search_output");
 
-// asynchronously load search "index" (the search box will remain disabled until then)
 let searchIndex;
+
+let searchResultsCount = 0;
+let searchSelection = -1;
+
+// asynchronously load search "index" (the search box will remain disabled until then)
 fetch("search.json")
     .then(response => response.json())
     .then(data => {
@@ -46,20 +50,31 @@ function search(query) {
 }
 
 function clearResults() {
+    searchResultsCount = 0;
+    searchSelection = -1;
+
     searchOutput.innerHTML = "";
 }
 
 // render a subset of the search index in the results/output pane
 function showResults(results) {
+    searchResultsCount = results.length;
+    searchSelection = -1;
+
+    let i = 0;
     const code = results.map(e => {
-        return `<h3><span class="icons">`
+        return `<a href="${e.htmlfile}" class="searchresult" id="${i++}">`
+            + `<h3>`
+            + `<i class="icons">`
             + (e.favorite ? `<img src="assets/tabler-icons/tabler-icon-star.svg"> ` : ``)
             + (e.spicy ? `<img src="assets/tabler-icons/tabler-icon-flame.svg"> ` : ``)
             + ((e.veggie || e.vegan) ? `` : `<img src="assets/tabler-icons/tabler-icon-bone.svg"> `)
             + (e.vegan ? `<img src="assets/tabler-icons/tabler-icon-leaf.svg"> ` : ``)
-            + `</span><a href="${e.htmlfile}">${e.title}</a> `
+            + `</i>`
+            + `<span>${e.title}</span> `
             + (e.original_title ? `<em>${e.original_title}</em>` : ``)
-            + `</h3>`;
+            + `</h3>`
+            + `</a>`;
     });
 
     searchOutput.innerHTML = code.join("");
@@ -72,4 +87,32 @@ searchInput.addEventListener('input', e => {
         const results = search(searchInput.value);
         showResults(results);
     }
+});
+
+// highlight the currently selected search result
+function highlightSearchSelection() {
+    document.querySelectorAll(".searchresult").forEach(e => e.classList.remove("selected"));
+    if (document.getElementById(`${searchSelection}`)) {
+        document.getElementById(`${searchSelection}`).classList.add("selected");
+    }
+}
+
+// allow keyboard naviation of search results
+searchInput.addEventListener('keydown', e => {
+    if (e.key == "ArrowUp") {
+        searchSelection = Math.max(-1, searchSelection - 1);
+    } else if (e.key == "ArrowDown") {
+        searchSelection = Math.min(searchResultsCount - 1, searchSelection + 1);
+    } else if (e.key == "Enter") {
+        if (searchSelection != -1) {
+            document.getElementById(`${searchSelection}`).click();
+        }
+    }
+    highlightSearchSelection();
+})
+
+// allow, in conjunction with keyboard naviation (otherwise this would be a job for css), highlighting on mouseover
+searchOutput.addEventListener('mousemove', e => {
+    searchSelection = parseInt(e.target.closest("a.searchresult").id);
+    highlightSearchSelection();
 });
